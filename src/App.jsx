3563ea0ccba7474cc1404910c4653f5223e710fc
wkkkis <ishakishmahametov123@gmail.com>
2021-11-26 22:58:10 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.scss";
+
+function App() {
+    return (
+        <div>
+            hello react webpack
+        </div>
+    )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+import App from "./App.jsx";
+
+render(<App />, document.getElementById("root"));
